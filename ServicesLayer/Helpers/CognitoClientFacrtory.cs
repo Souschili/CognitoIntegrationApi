9505ddba3ef447c8a35e24ec889c7e1a62034ec0
Orkhan <aliyevorhan82@gmail.com>
2024-09-15 20:35:08 +0400

    add factory method

---
+++ ServicesLayer/Helpers/CognitoClientFacrtory.cs
@@ -0,0 +1,32 @@
+﻿using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+
+
+namespace ServicesLayer.Helpers
+{
+    internal static class CognitoClientFacrtory
+    {
+        private static AmazonCognitoIdentityProviderClient _client;
+        private static readonly object _lock = new object();
+        // temp
+        static private readonly string _path = Path.Combine(Directory.GetCurrentDirectory(), "credential");
+
+        public static AmazonCognitoIdentityProviderClient GetClient()
+        {
+            if (_client != null)
+                return _client;
+            lock (_lock)
+            {
+                var chain = new CredentialProfileStoreChain(_path);
+
+                _client = chain.TryGetAWSCredentials("default", out var credential) ?
+                                new AmazonCognitoIdentityProviderClient(credential, RegionEndpoint.USEast1) :
+                                throw new AmazonClientException("Unable read AWS credentials");
+
+            }
+            return _client;
+        }
+    }
+}
