410c6d4e325c2db724cefaff1ac67e2b95de909a
Orkhan <aliyevorhan82@gmail.com>
2024-09-16 21:44:06 +0400

    add class wich implement interface

---
+++ ServicesLayer/Services/AdminCognitoUserService.cs
@@ -0,0 +1,23 @@
+﻿using ServicesLayer.DTOs;
+using ServicesLayer.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLayer.Services
+{
+    internal class AdminCognitoUserService : IAdminCognitoUserService
+    {
+        public Task AdminSetPasswordAsync(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AdminSignUpUserAsync(SignUpRequestDto signUpRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
