a82406ce6c7176483c6f27b7e77ecc226904a3e0
Orkhan <aliyevorhan82@gmail.com>
2024-10-04 04:30:05 +0400

    add dto for signIn

---
+++ ServicesLayer/DTOs/SignInRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ServicesLayer.DTOs
+{
+    public record class SignInRequest
+    {
+        public required string Login { get; init; }
+        public required string Password { get; init; }
+    }
+}
